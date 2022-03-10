af797b157db4511e1d7e575686d20d0d3f185359
jcabe <jcabe@DESKTOP-75RMB14>
2022-03-10 20:49:27 +0100

    act

--- src/calculadoraNotas/App.java
+++ src/calculadoraNotas/App.java
@@ -7,7 +7,7 @@ public class App {
 		//PROGRAMACION
 		double[] notasExamenesProgramacion = {10};
 		double[] notasPracticasProgramacion = {10, 10, 10, 10, 10, 10, 9.75, 9.5, 9.5, 10, 9.65, 9, 10, 10, 8};
-		int notaActitudProgramacion = 7;
+		int notaActitudProgramacion = 5;
 		Asignatura programacion = new Asignatura("Programacion", notasExamenesProgramacion, notasPracticasProgramacion, notaActitudProgramacion, 0.70, 0.20, 0.10);
 		
 		System.out.println(programacion.toString());
@@ -15,8 +15,8 @@ public class App {
 		//LENGUAJES DE MARCAS
 		
 		double[] notasExamenesLenguajes = {10};
-		double[] notasPracticasLenguajes = {9};
-		int notaActitudLenguajes = 7;
+		double[] notasPracticasLenguajes = {5};
+		int notaActitudLenguajes = 5;
 		
 		Asignatura lenguajes = new Asignatura("Lenguajes de marcas", notasExamenesLenguajes, notasPracticasLenguajes, notaActitudLenguajes, 0.60, 0.30, 0.10);
 		
@@ -26,7 +26,7 @@ public class App {
 		
 		double[] notasExamenesSistemas = {10};
 		double[] notasPracticasSistemas = {10, 10, 10, 10, 10, 10, 10, 10, 10};
-		int notaActitudSistemas = 7;
+		int notaActitudSistemas = 5;
 		
 		Asignatura sistemas = new Asignatura("Sistemas informaticos", notasExamenesSistemas, notasPracticasSistemas, notaActitudSistemas, 0.70, 0.20, 0.10);
 		
@@ -35,8 +35,8 @@ public class App {
 		//INGLES
 		
 		double[] notasExamenesIngles = { 7, 7.17, 7.77, 7.95};
-		double[] notasPracticasIngles = {10};
-		int notaActitudIngles = 7;
+		double[] notasPracticasIngles = {5};
+		int notaActitudIngles = 5;
 		
 		Asignatura ingles = new Asignatura("Ingles", notasExamenesIngles, notasPracticasIngles, notaActitudIngles, 0.90, 0.00, 0.10);
 		
@@ -46,7 +46,7 @@ public class App {
 		
 		double[] notasExamenesBases = {10};
 		double[] notasPracticasBases = {8.75, 8.56, 8.4, 6.9, 10, 10, 10};
-		int notaActitudBases = 7;
+		int notaActitudBases = 5;
 		
 		Asignatura bases = new Asignatura("Bases de datos", notasExamenesBases, notasPracticasBases, notaActitudBases, 0.80, 0.15, 0.05);
 		
@@ -54,9 +54,9 @@ public class App {
 		
 		//ENTORNOS DE DESARROLLO
 		
-		double[] notasExamenesEntornos = {10};
+		double[] notasExamenesEntornos = {7.6};
 		double[] notasPracticasEntornos = {10, 8.43, 10, 9.5, 8, 10, 10, 10, 10};
-		notaActitudBases = 10;
+		notaActitudBases = 5;
 		
 		Asignatura entornos = new Asignatura("Entornos de desarollo", notasExamenesEntornos, notasPracticasEntornos, notaActitudBases, 0.65, 0.30, 0.05);
 		
